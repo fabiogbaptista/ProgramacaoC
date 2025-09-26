14eff8fb892e3432e3e4ae6d2f211e5c0a44789d
fabiogbaptista <fabiogbaptista@gmail.com>
2025-09-26 19:35:02 +0000

    operadores_logicos

---
+++ operadores_logicos.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+int main() {
+
+    int idade = 10;
+    float altura = 1.20;
+
+    if (idade >=18 && idade <=70 || altura >=1.80) {
+        printf("a pessoa pode andar na montanha russa.\n");
+    }
+    else 
+    {
+        printf("a pessoa nao pode andar na montanha russa.\n");
+    }
+    return 0;
+}
+ 
