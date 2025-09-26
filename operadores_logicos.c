f85c517e982ffb9b7179644c9eacc50e75bbb6b4
fabiogbaptista <fabiogbaptista@gmail.com>
2025-09-26 19:38:02 +0000

    usando negacao de condicao logica

--- operadores_logicos.c
+++ operadores_logicos.c
@@ -4,7 +4,7 @@ int main() {
     int idade = 10;
     float altura = 1.20;
 
-    if (idade >=18 && idade <=70 || altura >=1.80) {
+    if (!(idade >=18 && idade <=70 || altura >=1.80)) {
         printf("a pessoa pode andar na montanha russa.\n");
     }
     else 
