dc3cbea9df1820fece2fe43447b06284a7965d18
fabiogbaptista <fabiogbaptista@gmail.com>
2025-10-31 22:40:25 +0000

    movendo cavalo

--- xadrez.c
+++ xadrez.c
@@ -3,6 +3,7 @@ int main(){
 
     // Declarando as variáveis
     int movtorre =5, movbispo = 5, movrainha = 8;
+    int moviementocompleto = 1;
 
     // Lógica do movimento da torre
     printf("Movendo a torre:\n");
@@ -22,6 +23,19 @@ int main(){
     printf("Movendo a rainha:\n");
     for (int i = 0; i < movrainha; i++) {
         printf("Esquerda.\n");
+    }
+
+    //Lógica do movimento do cavalo
+    printf("Movendo o cavalo:\n");
+
+    while(moviementocompleto--){
+
+        for (int i=0; i < 2; i++)
+        {
+            printf("Cima.\n");
+        }
+        printf("Direita.\n");
+
     }
     return 0;
 }
